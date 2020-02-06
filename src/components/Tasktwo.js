3c0d2e649abc785b52cc3351626f702853dd3ff4
aesher9o1 <aashiskumar986@gmail.com>
2020-02-06 23:33:22 +0530

    Bottom texts added

--- src/components/Tasktwo.js
+++ src/components/Tasktwo.js
@@ -11,8 +11,20 @@ function Tasktwo() {
                 <br />
                 <div >
                     <i className="hours">HOURS</i>
-                    <i className="hours of">OF...</i>
+                    <i className="hours of">OF ...</i>
                 </div>
+            </div>
+            <div className="bottom-section">
+                <h1>DESIGNING / BUILDING / CODING / HACKING</h1>
+                <h2>NETWORKING / FRIENDS / MENTORS / COMPETITIONS</h2>
+                <h3>COFFEE / TEA / GREEN TEA / FOOD / SNACKS / SWAGS / T-SHIRTS</h3>
+                <h4>SUPER FAST INTERNET / TALKS / DID WE MENTION GREEN TEA? / PRIZES / BRAND NEW APIs</h4>
+
+                <h5>AND / A / WHOLE / LOT / MORE
+                    <hr className="underline" />
+                </h5>
+
+
             </div>
         </div>
     )
