9f01b368ee6369dd347acc9ee09da976f3f802df
aesher9o1 <aashiskumar986@gmail.com>
2020-02-06 23:04:30 +0530

    Added 30 hours of

--- src/components/Tasktwo.js
+++ src/components/Tasktwo.js
@@ -3,20 +3,17 @@ import './Tasktwo.css'
 
 function Tasktwo() {
     return (
-        <div className="card tasktwo" >
-            <section style={{ position: "relative" }}>
-                <section style={{ margin: "0 auto 0 auto", position: 'absolute' }}>
-                    <p>
-                        <i className="num-hours">30</i>
-                        <br />
-                        <i className="hours">
-                            HOURS<br />
-                            <i className="of">OF...</i>
-                        </i>
-
-                    </p>
-                </section>
-            </section>
+        <div id="main_content" >
+            <div id="container">
+                <div className="number">
+                    30
+                </div>
+                <br />
+                <div >
+                    <i className="hours">HOURS</i>
+                    <i className="hours of">OF...</i>
+                </div>
+            </div>
         </div>
     )
 }
